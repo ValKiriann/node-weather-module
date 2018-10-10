da2842ab373a935465158304253aadba2758703d
ValKiriann <hello@anaenriquez.es>
2018-10-10 22:07:56 +0000

    Finished the logic

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.listen(port, function () {
     app.get('/compareWeather/madrid', async (req, res, next) => {
         try {
             if(!req.query.city){
-                res.status(423).send("Please, add a city compare with");
+                var errorLog = {type:"Server error", message:`Server error: Please enter a city to compare. Make sure that the query has city as parameter`}
+                res.status(423).send(temps.responseHandler(423,null,errorLog));
             }else {
-                var city1Temp = await temps.getTemp('madrid')
-                var city2Temp = await temps.getTemp(req.query.city)
-                res.status(200).send(`The current temperature in Madrid is ${city1Temp} and in ${req.query.city} is ${city2Temp}`);
+                var result = await temps.compareTemps("madrid",req.query.city)
+                res.status(result.httpStatusCode).json(result);
             }
         }
         catch(error) {
@@ -35,15 +35,20 @@ app.listen(port, function () {
     app.get('/compareWeather', async (req, res, next) => {
         try {
             if(!req.query.city1 || !req.query.city2){
-                res.status(423).send("Please, make sure that you have added two cities to compare with");
+                var errorLog = {type:"Server error", message:`Server error: Please enter 2 cities to compare. Make sure that the query has city1 and city2 as parameters`}
+                res.status(423).send(temps.responseHandler(423,null,errorLog));
             }else {
-                var city1Temp = await temps.getTemp(req.query.city1)
-                var city2Temp = await temps.getTemp(req.query.city2)
-                res.status(200).send(`The current temperature in ${req.query.city1} is ${city1Temp} and in ${req.query.city2} is ${city2Temp}`);
+                var result = await temps.compareTemps(req.query.city1,req.query.city2)
+                console.log(result)
+                res.status(result.httpStatusCode).json(result);
             }
         }
         catch(error) {
             console.log('catch error ', error)
         }
     })
+    app.get('*', function(req, res){
+        var errorLog = {type:"Not Found", message:`Not found: Nothing to do here`}
+        res.status(404).send(temps.responseHandler(404,null,errorLog));
+    });
 });
--- temps.js
+++ temps.js
@@ -8,22 +8,64 @@ var temps = {
             let response = await fetch(url)
             let data = await response.json();
             if(data.cod == 200) {
-                console.log(url)
-                console.log(`returning ${city} temp: ${data.main.temp}`)
                 return data.main.temp
             }else if(data.cod == '404'){
-                console.log('bad city spelling ', city)
-                console.log(url)
-                console.log(data)
+                var error = {type:"Invalid city", message:"City name is invalid or does not exist."}
+                return temps.responseHandler(422,null,error)
             }else {
-                console.log(url)
-                console.log(data)
+                var error = {type:"Server error", message:`Server error: ${data.message}`}
+                return temps.responseHandler(500,null,error)
             }
         } catch(error) {
-            console.log('error ', error)
+            //console.log('getTemp catch error ', error)
+            var errorLog = {type:"Server error", message:`Server error: ${error}`}
+            return temps.responseHandler(500,null,errorLog)
         }
+    },
+    compareTemps: async (city1,city2) => {
+        try{
+            var tempCity1 = await temps.getTemp(city1)
+            console.log('compruebo city1')
+            if(typeof tempCity1 == 'object'){
+                
+                return tempCity1
+            }
+            var tempCity2 = await temps.getTemp(city2)
+            console.log('compruebo city2')
+            if(typeof tempCity2 == 'object'){
+                return tempCity2
+            }
+            var tempDiff = tempCity1 - tempCity2
+            console.log("comparo las temp")
+            if(tempDiff > 0) {
+                var result = {
+                    warmerCityName: city1,
+                    colderCityName: city2,
+                    difference: tempDiff
+                }
+                return temps.responseHandler(200,result)
+            }else {
+                tempDiff = Math.abs(tempDiff)
+                var result = {
+                    warmerCityName: city2,
+                    colderCityName: city1,
+                    difference: tempDiff
+                }
+                return temps.responseHandler(200,result)
+            }
 
-        
+        } catch(error) {
+            //console.log('compaTemp catch error ', error)
+            var errorLog = {type:"Server error", message:`Server error: ${error}`}
+            return temps.responseHandler(500,null,errorLog)
+        }
+    },
+    responseHandler: function(statuscode, result, errors){
+        let jsonResult = { httpStatusCode: statuscode}
+        if(result){jsonResult.result = result}
+        if(errors){jsonResult.errors = errors}
+        console.log('jsonResult ', jsonResult)
+        return jsonResult
     }
 }
 
